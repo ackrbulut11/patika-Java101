a0fe6c38909eda4fec7fe30fa25cc235ba5b60a1
Ahmetcan Karabulut <ackrbulut11@gmail.com>
2024-04-05 16:04:15 +0300

    Body Mass Index

---
+++ BodyMassIndex/BMI.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class BMI{
+    
+    public static String bmiHesapla() {
+        Scanner input = new Scanner(System.in);
+    
+        System.out.print("Kilonuzu girin (kg): ");
+        int kg = input.nextInt();
+        
+        System.out.print("Boyunuzu girin (cm): ");
+        int boy = input.nextInt();
+        
+        double bmiDeger = kg / (((double)boy / 100) * ((double)boy / 100));
+        String bmi;
+    
+        if (bmiDeger <= 18.5){
+            bmi = "Underweight";
+        } else if(bmiDeger < 25){
+            bmi = "Normal";
+        } else if (bmiDeger < 30){
+            bmi = "Overweight";
+        } else if (bmiDeger < 35){
+            bmi = "Obese";
+        } else {
+            bmi = "Extremely Obese";
+        }
+    
+        return   "\n" + bmi + "\n" +  "Vücut kitle indeksi: " + String.format("%.2f",bmiDeger);
+    }
+    
+    public static void main(String[] args){
+        
+        System.out.println( BMI.bmiHesapla());
+        
+    }
+}
---
+++ BodyMassIndex/README.md
@@ -0,0 +1,4 @@
+#### Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atadım. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırdım.
+
+# Formül
+#### Kilo (kg) / Boy(m) * Boy(m)
