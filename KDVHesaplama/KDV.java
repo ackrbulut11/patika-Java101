a2399ebbfb3d6525e0a31ad807029ae62fa7c2b6
Ahmetcan Karabulut <ackrbulut11@gmail.com>
2024-04-05 03:04:42 +0300

    KDV Hesaplama

---
+++ KDVHesaplama/KDV.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class KDV {
+    public static void main(String[] args) throws Exception {
+
+        double fiyat;
+        double vergi;
+
+        System.out.print("Urunun fiyatini girin: ");
+        Scanner input = new Scanner(System.in);
+        fiyat = input.nextDouble();
+
+        if (fiyat >= 1000){
+            vergi = fiyat * 8/100;
+            fiyat += vergi;
+            System.out.printf("Urunun KDV tutari: %.2f\n", vergi);
+            System.out.printf("Toplam tutar: %.2f\n", fiyat);
+
+        } else {
+            vergi = fiyat * 18/100;
+            fiyat += vergi;
+            System.out.printf("Urunun KDV tutari: %.2f\n", vergi);
+            System.out.printf("Toplam tutar: %.2f\n", fiyat);
+        }
+    }
+}
