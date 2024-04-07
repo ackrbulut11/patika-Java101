41987601f3eee96aca72bacc685e85523f0b11de
Ahmetcan Karabulut <ackrbulut11@gmail.com>
2024-04-08 00:13:09 +0300

    Update AktiviteTavsiye.java

--- AktiviteTavsiye/AktiviteTavsiye.java
+++ AktiviteTavsiye/AktiviteTavsiye.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AktiviteTavsiye {
+    @SuppressWarnings("resource")
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
